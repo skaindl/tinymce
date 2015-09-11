e0bf96f7a6e2a06c7477137237c2edf29bc241f0
skaindl <skaindl@users.noreply.github.com>
2015-09-11 11:16:07 +0200

    Update EditorUpload.js

--- js/tinymce/classes/EditorUpload.js
+++ js/tinymce/classes/EditorUpload.js
@@ -117,11 +117,11 @@ define("tinymce/EditorUpload", [
 			}
 
 			return imageScanner.findAll(editor.getBody()).then(aliveGuard(function(result) {
-				Arr.each(result, function(resultItem) {
-					// Don't replace base64 img src with blob
+				// Don't replace base64 img src with blob
+				//Arr.each(result, function(resultItem) {
 					// replaceUrlInUndoStack(resultItem.image.src, resultItem.blobInfo.blobUri());
 					// resultItem.image.src = resultItem.blobInfo.blobUri();
-				});
+				//});
 
 				return result;
 			}));
